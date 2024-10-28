a05a039040b7e274fe3d07810fa1383b96b3556f
Yalanskyi Roman <keito.eternal@gmail.com>
2024-10-28 20:45:56 +0200

    adding only guest chat

--- pages/auth/index.tsx
+++
@@ -1,6 +0,0 @@
-// pages/auth/index.tsx
-import AuthComponent from '../../src/components/AuthComponent';
-
-export default function AuthPage() {
-  return <AuthComponent />;
-}
--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -8,71 +8,71 @@ import '../styles/authComponent.css';
 
 const AuthComponent = () => {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLogin, setIsLogin] = useState(true);
-  const [message, setMessage] = useState<string | null>(null);
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [isLogin, setIsLogin] = useState(true);
+  // const [message, setMessage] = useState<string | null>(null);
 
-  const handleSignUp = async () => {
-    if (!email || !password) {
-      setMessage('Please fill in both email and password to sign up.');
-      return;
-    }
+  // const handleSignUp = async () => {
+  //   if (!email || !password) {
+  //     setMessage('Please fill in both email and password to sign up.');
+  //     return;
+  //   }
 
-    try {
-      await signUp(email, password);
-      // setMessage('Sign-up successful! Redirecting...');
-      router.push('/');
-    } catch (error) {
-      console.error(error);
-      setMessage('Sign-up failed! Please check your details and try again.');
-    }
-  };
+  //   try {
+  //     await signUp(email, password);
+  //     // setMessage('Sign-up successful! Redirecting...');
+  //     router.push('/');
+  //   } catch (error) {
+  //     console.error(error);
+  //     setMessage('Sign-up failed! Please check your details and try again.');
+  //   }
+  // };
 
-  const handleLogIn = async () => {
-    if (!email || !password) {
-      setMessage('Both email and password are required to log in.');
-      return;
-    }
+  // const handleLogIn = async () => {
+  //   if (!email || !password) {
+  //     setMessage('Both email and password are required to log in.');
+  //     return;
+  //   }
 
-    try {
-      await logIn(email, password);
-      // setMessage('Login successful! Redirecting...');
-      router.push('/');
-    } catch (error) {
-      console.error(error);
-      if (error.code === 'auth/user-not-found') {
-        setMessage('No account found with this email. Please sign up first.');
-      } else if (error.code === 'auth/wrong-password') {
-        setMessage('Incorrect password. Please try again.');
-      } else {
-        setMessage('Login failed! Please check your details and try again.');
-      }
-    }
-  };
+  //   try {
+  //     await logIn(email, password);
+  //     // setMessage('Login successful! Redirecting...');
+  //     router.push('/');
+  //   } catch (error) {
+  //     console.error(error);
+  //     if (error.code === 'auth/user-not-found') {
+  //       setMessage('No account found with this email. Please sign up first.');
+  //     } else if (error.code === 'auth/wrong-password') {
+  //       setMessage('Incorrect password. Please try again.');
+  //     } else {
+  //       setMessage('Login failed! Please check your details and try again.');
+  //     }
+  //   }
+  // };
 
   const handleGuestLogin = async () => {
-    try {
-      await continueAsGuest();
+    // try {
+      // await continueAsGuest();
       // setMessage('Continuing as guest...');
-      router.push('/');
-    } catch (error) {
-      console.error(error);
-      setMessage('Guest login failed! Please try again later.');
-    }
+    router.push('/guest');
+    // } catch (error) {
+    //   console.error(error);
+    //   setMessage('Guest login failed! Please try again later.');
+    // }
   };
 
   return (
     <div className='authContainer'>
       <div className='authOptionsWrapper'>
         <h1 className='authPageTitle'>
-          <BotResponse response="Authorize in Handwriting recognition bot or continue as guest" />
+          <BotResponse response="Welcome to Handwriting recognition bot" />
         </h1>
         <h2 className='authPageDetails'>
-        By picking guest authorisation  <BotResponse response="your chat data won't be saved and you will only have one chat avaliable." />
+        You can continue as guest using button below, <BotResponse response="your chat data won't be saved and you will only have one chat avaliable." />
         </h2>
         
-        <div className='authInputsWrapper'>
+        {/* <div className='authInputsWrapper'>
           <input
             type="email"
             placeholder="Email"
@@ -88,13 +88,13 @@ const AuthComponent = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           {message && <div className='authMessage'>{message}</div>}
-        </div>
+        </div> */}
         
 
 
         <div className='authButtonsWrapper'>
           <div className='authButtonsBlock'>
-            <button
+            {/* <button
               className='authButton'
               onClick={isLogin ? handleLogIn : handleSignUp}
             >
@@ -108,7 +108,7 @@ const AuthComponent = () => {
               }}
             >
               {isLogin ? 'Switch to Sign Up' : 'Switch to Log In'}
-            </button>
+            </button> */}
             <button
               className='authAsGuestButton'
               onClick={handleGuestLogin}
