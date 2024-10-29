4ef9f3a19f90306d383f8a411c350f07baba92dc
Yalanskyi Roman <keito.eternal@gmail.com>
2024-10-29 10:22:37 +0200

    added custom icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '../src/styles/globals.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -6,6 +7,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="icon"
+            href="/icon192.png"
+            type="image/png"
+            sizes="192x192"
+          />
+          <link
+            rel="icon"
+            href="/icon512.png"
+            type="image/png"
+            sizes="512x512"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/apple-icon.png"
+            type="image/png"
+            sizes="any"
+          />
+      </head>
       <body>{children}</body>
     </html>
   )
